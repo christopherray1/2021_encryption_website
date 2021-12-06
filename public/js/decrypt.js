679326bf02d90a3963e1ef41755cd00f4bb27f9f
christopherray1 <78582779+christopherray1@users.noreply.github.com>
2021-12-05 19:39:57 -0600

    Add files via upload
    Added Polybius Decryption with standard error checks

--- public/js/decrypt.js
+++ public/js/decrypt.js
@@ -7,6 +7,18 @@
 var originalAlphabet = {0:"a", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7:"h", 8:"i", 9:"j", 10:"k",
                        11:"l", 12:"m", 13:"n", 14:"o", 15:"p", 16:"q", 17:"r", 18:"s", 19:"t", 20:"u", 21:"v", 22:"w", 23:"x", 24:"y", 25:"z"};
 
+// var polybiusSquareRowValues = {"a": 1, "b": 1, "c": 1, "d": 1, "e": 1, "f": 2, "g": 2, "h": 2, "i": 2, "j": 2, "k": 3, "l": 3, "m": 3, "n": 3,
+                              // "o": 3, "p": 4, "q": 4, "r": 4, "s": 4, "t": 4, "u": 5, "v": 5, "w": 5, "x": 5, "z": 5};
+
+// var polybiusSquareColumnValues = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 1, "g": 2, "h": 3, "i": 4, "j": 5, "k": 1, "l": 2, "m": 3, "n": 4,
+                                 // "o": 5, "p": 1, "q": 2, "r": 3, "s": 4, "t": 5, "u": 1, "v": 2, "w": 3, "x": 4, "z": 5};
+
+// Kept these here for my own reference I wasnt quite sure if I understood how this cypher worked.
+
+var polybiusSquareReverseKey = { 11 : "a" , 12 : "b", 13 : "c", 14 : "d", 15 : "e", 21 : "f", 22 : "g",23 : "h", 24 : "i", 25 : "j", 31 : "k", 32 : "l", 33 : "m", 
+                                 34 : "n", 35 : "o", 41 : "p", 42 : "q", 43 : "r", 44 : "s", 45 : "t", 51 : "u", 52 : "v", 53 : "w", 54 : "x", 55 : "y", 61 : "z"}; 
+                                 // This is the number pair of each of the letters for this method of encryption
+
 function caesarDecrypt() {
 
   var textToDecrypt = [];  // Array to pass words/word to, to decrypt
@@ -53,8 +65,57 @@ function caesarDecrypt() {
       }
       encryptedText += " "; // add a space at the end of the word, incase there are multiple words
 
-  });
-  
+  });  
+
   outputText.innerHTML = encryptedText; // Set our output test box equal to our encrypted text
   
+};
+
+function polybiusDecrypt() {
+
+    var textToDecrypt = (document.querySelector("#inputPolybiusDecrypt").value); // takes text from 'encrypted text' box
+    var textDecryptArray = []; // Allows the input to be grouped into sets of two
+    var outputTextBox = document.querySelector("#outputPolybiusDecrypt"); // targets the 'clear text' output box
+    var decryptedText = ""; // setup our output message
+
+    if (/^[a-zA-Z]+$/.test(textToDecrypt)) { // Checks to see if you typed anything that was a letter 
+
+        decryptedText = "Please only use number pairs that coorilate with a letter. (No letters, or non numeric charecters)";
+
+    }
+
+    
+
+    for (var i = 0; i<textToDecrypt.length-1; i+=2) { // Takes the input text and puts it in groups of two, allowing us to match those number pairings with the above key array
+
+        textDecryptArray.push((textToDecrypt[i]+''+textToDecrypt[i+1]));
+
+       }
+
+    for (var i =0; i < textDecryptArray.length; i++) { // Step through each letter in the input text
+        
+        decryptedText += polybiusSquareReverseKey[textDecryptArray[i]]; // matches each number pair with its letter value
+        
+    }
+
+    if (decryptedText.includes('undefined')) { // Checks for number pairs that wouldnt have a letter (aka wrong number pairs)
+
+        decryptedText = "Please only use number pairs that coorilate with a letter. (No letters, or non numeric charecters)";
+
+    }
+
+         
+
+    outputTextBox.innerHTML = decryptedText; // set our output message
+
+
+    function checkIfLetterInArraysP(letter, rowsArray) { // function to check if current letter is in our polybius square, we only check if it is in the rows, because
+                                                         // this covers all possible letters
+        if (rowsArray[letter]) { // Return true if our letter is in the polybius square
+            return true;
+        } else { // every other character, return false. Meaning y or a number or (),&,^,% etc.
+            return false;
+        }
+
+    }
 };
