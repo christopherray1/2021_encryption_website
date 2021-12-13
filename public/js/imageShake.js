cdbc331072e893c79eaf4d1796102d4f479c63a4
mmelland2 <mmelland@outlook.com>
2021-12-12 22:13:44 -0600

    [Finishes #180323490] Cleaned up leftover code

--- public/js/imageShake.js
+++ public/js/imageShake.js
@@ -1,6 +1,35 @@
 $(function() {
+
+  $count = 0;
+
+  $oldSrc = 'images/logo5.png';
+  $newSrc = 'images/logo5broken.png';
+
   $('#imgLock').click(function() {
-    $('#imgLock').stop().effect( "bounce", {times:8}, 500 );
+    $count += 1
+
+    if ($count == 100) {
+      alert("Why are you like this");
+      $('img[src="' + $oldSrc + '"]').attr('src', $newSrc);
+      $(this).stop()
+  
+    } else if ($count > 100) {
+      //Do nothing
+      
+    } else {
+      $(this).stop().effect( "bounce", {times:8}, 500 );
+    };
+
+
+    console.log('Count', $count);
+    
+    
+
+
   });
+
+
+
+  
 
 });
